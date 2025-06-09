0d64d5e2f74c830d68aa8ecaff5dfa57d42c5677
Rechcel Toledo <rechceltoledo@gmail.com>
2025-06-10 05:24:32 +0800

    updated: credit report component

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_URL = import.meta.env.VITE_API_URL || 'https://acenstra-backend-production.up.railway.app'; // Point to backend
-const API_URL = 'http://localhost:5000';  
+const API_URL = import.meta.env.VITE_API_URL || 'https://acenstra-backend-production.up.railway.app'; // Point to backend
+//const API_URL = 'http://localhost:5000';  
 
 
 const apiClient = axios.create({
